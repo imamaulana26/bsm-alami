6bff73cf8efb31fd65f361e7ee4e3a2ad755f027
Imam Maulana Ibrahim <imamaulana26.github@gmail.com>
2020-09-25 13:39:22 +0700

    prod mode

--- application/config/webconfig.php
+++ application/config/webconfig.php
@@ -1,26 +1,26 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-$ver = "envi"; // envi or prod or maintenance
+$ver = "prod"; // envi or prod or maintenance
 
 switch ($ver) {
 	case "maintenance":
 		$config = array(
-			'version' => '',
-			'hostname' => '',
-			'username' => '',
-			'password' => '',
-			'database' => '',
+			'version' => 'maintenance',
+			'hostname' => 'localhost',
+			'username' => 'u6020540_root',
+			'password' => 'maulana26',
+			'database' => 'u6020540_db_alami',
 			'dbdriver' => 'mysqli'
 		);
 		break;
 	case "prod":
 		$config = array(
-			'version' => '',
-			'hostname' => '',
-			'username' => '',
-			'password' => '',
-			'database' => '',
+			'version' => 'prod',
+			'hostname' => 'localhost',
+			'username' => 'u6020540_root',
+			'password' => 'maulana26',
+			'database' => 'u6020540_db_alami',
 			'dbdriver' => 'mysqli'
 		);
 		break;
