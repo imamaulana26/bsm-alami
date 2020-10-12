7578f8b6541fbe96b9b21c54472257c3fc84a717
Imam Maulana Ibrahim <50631091+imamaulana26@users.noreply.github.com>
2020-10-12 18:48:14 +0700

    Create webconfig.php

---
+++ application/config/webconfig.php
@@ -0,0 +1,37 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+$ver = "prod"; // envi or prod or maintenance
+
+switch ($ver) {
+	case "maintenance":
+		$config = array(
+			'version' => 'maintenance',
+			'hostname' => 'localhost',
+			'username' => 'u6020540_root',
+			'password' => 'maulana26',
+			'database' => 'u6020540_db_alami',
+			'dbdriver' => 'mysqli'
+		);
+		break;
+	case "prod":
+		$config = array(
+			'version' => 'prod',
+			'hostname' => 'localhost',
+			'username' => 'u6020540_root',
+			'password' => 'maulana26',
+			'database' => 'u6020540_db_alami',
+			'dbdriver' => 'mysqli'
+		);
+		break;
+	default:
+		$config = array(
+			'version' => 'envi',
+			'hostname' => 'localhost',
+			'username' => 'root',
+			'password' => '',
+			'database' => 'db_bsm_alami',
+			'dbdriver' => 'mysqli'
+		);
+		break;
+}
